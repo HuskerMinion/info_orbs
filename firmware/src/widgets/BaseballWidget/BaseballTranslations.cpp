468392f8e7f9a3d3362f3c4bdb89c69257c8b8fc
David Reed <212698+dreed47@users.noreply.github.com>
2025-04-04 15:46:01 -0400

    added baseball widget

---
+++ firmware/src/widgets/BaseballWidget/BaseballTranslations.cpp
@@ -0,0 +1,31 @@
+#include "BaseballTranslations.h"
+
+constexpr Translation t_baseballShortName = {
+    "Team Name", // EN
+    nullptr, // DE
+    nullptr, // FR
+};
+
+constexpr Translation t_baseballTeamName = {
+    "Team Information", // EN
+    nullptr, // DE
+    nullptr, // FR
+};
+
+constexpr Translation t_baseballLastGame = {
+    "Last Game", // EN
+    nullptr, // DE
+    nullptr, // FR
+};
+
+constexpr Translation t_baseballNextGame = {
+    "Next Game", // EN
+    nullptr, // DE
+    nullptr, // FR
+};
+
+constexpr Translation t_baseballStandings = {
+    "Standings", // EN
+    nullptr, // DE
+    nullptr, // FR
+};
---
+++ firmware/src/widgets/BaseballWidget/BaseballTranslations.h
@@ -0,0 +1,12 @@
+#ifndef BASEBALLTRANSLATIONS_H
+#define BASEBALLTRANSLATIONS_H
+
+#include "I18n.h"
+
+extern Translation t_baseballTeamName;
+extern Translation t_baseballLastGame;
+extern Translation t_baseballNextGame;
+extern Translation t_baseballStandings;
+extern Translation t_baseballShortName;
+
+#endif
